1335fea85aab8a7adbb8f0a1d2d51b2e8d900644
Priyaranjan2005 <priyaranjanpradhan031@gmail.com>
2021-04-26 12:15:25 +0530

    Bob added

---
+++ Bob.js
@@ -0,0 +1,24 @@
+class Bob {
+    constructor(x, y,radius) {
+      var options = {
+          'isStatic':false,
+          'restitution':0.3,
+          'friction':0.5,
+          'density':0.2
+      }
+      this.body = Bodies.circle(x, y,radius,options);
+      this.radius = radius;
+
+
+      World.add(world, this.body);
+    }
+
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      fill(255);
+      circle(0, 0, this.radius);
+      pop();
+    }
+  };
